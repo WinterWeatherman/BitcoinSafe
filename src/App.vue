d23ad34ff44e75c2199ee2c8318b13cadadb1da6
willweatherman <will@mathbot.com>
2021-03-29 12:56:42 -0400

    fixed mobile styleing

--- src/App.vue
+++ src/App.vue
@@ -77,4 +77,194 @@ html {
 .box-shadow { box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05); }
 
 
+.card-header-two {
+    padding: 4.25% 10%;
+    margin-bottom: 0;
+    background-color: #007bff;
+    border-bottom: 10% solid #007bff;
+    color: white;
+}
+
+.avatar{
+  margin-right: auto;
+  margin-left: auto;
+  vertical-align: middle;
+  width: 50%;
+  height: 50%;
+  border-radius: 50%;
+  display: block;
+  border: solid gray 1%;
+}
+
+.p {
+    margin-top: 0;
+    margin-bottom: 1rem;
+    display: block;
+}
+
+.grid{
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(11.8rem, 20fr));
+  grid-template-rows: auto;
+  gap: 2.5rem 2rem;
+  margin: auto;
+  width: 100%;
+  margin: auto;
+  padding-left: 2rem;
+  max-width: 56rem;
+}
+
+.borderstyle {
+    width: 100%;
+    max-width: 73.75rem;
+    margin: auto;
+    border-top: .25rem solid rgb(236, 239, 241);
+    padding: 1.75rem 1.25rem 2.5rem;
+}
+
+.grid-item {
+  height: 100%;
+  min-width: 0rem;
+  grid-column-end: span 1;
+  grid-row-end: span 1;
+  width: 11.8rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.grid-a {
+  color: rgba(17, 51, 83, 0.6);
+  cursor: pointer;
+  font-size: 1rem;
+  text-decoration: none;
+  transition: color 0.25s ease 0s;
+}
+
+.grid-p {
+  margin: 0rem;
+  font-size: 1rem;
+  font-weight: 400;
+  color: rgba(17, 51, 83, 0.6);
+  line-height: 1.5rem;
+  box-sizing: border-box;
+}
+
+.grid-p-2 {
+  margin: 0rem;
+  font-size: 1rem;
+  line-height: 2rem;
+  color: rgb(5, 15, 25);
+  font-weight: 600;
+}
+
+.grid-img {
+  width: 100%;
+  margin-bottom: 1.5rem;
+  border-radius: 50%;
+  border: 0.08rem solid rgb(236, 239, 241);
+  padding: 0.5rem;
+}
+
+.card-two {
+    position: relative;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #007bff;
+    background-clip: border-box;
+    border: 0rem solid rgba(0,0,0,.125);
+    border-radius: .25rem;
+}
+
+
+.custom-row{
+  display: flex;
+  flex: wrap;
+}
+
+.right{
+  margin-left: 0.6rem;
+}
+
+.left{
+  margin-right: 0.6rem;
+}
+
+.flex {
+  margin-right: auto;
+  margin-left: auto;
+  display: flex;
+  justify-content: center;
+}
+
+.center {
+  margin-right: auto;
+  margin-left: auto;
+  display: block;
+}
+
+.switch {
+  position: relative;
+  top: 0.25rem;
+  width: 3rem;
+  height: 1rem;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #2196F3;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 75%;
+  width: 25%;
+  bottom: 12.5%;
+  left: 4%;
+  background-color: white;
+  -webkit-transition: .3s;
+  transition: .3s;
+}
+
+.slider.round {
+  border-radius: 1.6rem;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 0.08rem #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(1.5rem);
+  -ms-transform: translateX(2rem);
+  transform: translateX(2rem);
+}
+
 </style>
